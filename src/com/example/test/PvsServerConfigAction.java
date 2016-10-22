11aff4294344d8015215dd1d55cd4b71fca1f71d
zhenjun115 <huangzhenjun115@hotmail.com>
2016-10-22 09:18:49 +0800

    init;

---
+++ src/com/example/test/PvsServerConfigAction.java
@@ -0,0 +1,10 @@
+package com.example.test;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class PvsServerConfigAction extends ActionSupport{
+	
+	public String editItem(){
+		return SUCCESS;
+	}
+}
